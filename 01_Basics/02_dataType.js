4b1110060081f65448658f6e5233511aff73c7b1
Sys-Sunny <142171902+Sys-Sunny@users.noreply.github.com>
2023-08-20 06:08:59 +0000

    Data Type

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS Code as newer version
+//alert(3+3)// we are using node js not browser 
+
+console.log(3+3) ;console.log("Sunny")
+//code readability should be high
+
+let name="Sunny"//string
+let age=18//integer
+let isLoggedIn=false//Bollean
+let state;//undefined
+//nuber=> 2 to [power 53]
+//bigInt
+//String=>""
+//boolean =>true/false
+//null=>standalone value
+//undefine=>
+//symbol =>unique
+
+
+//object
+console.log(typeof "sunny")
+console.log(typeof null)
+console.log(typeof undefined)
